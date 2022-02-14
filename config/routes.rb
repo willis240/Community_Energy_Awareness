a3e3d203a4cacc1ff96724d3188e7a216831696d
AlaskaBlake <swimmingblake@hotmail.com>
2022-02-14 12:43:50 -0900

    created mock website layout and awaiting code review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'website/Index'
-  get 'dashboard/index'
+  get 'website/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
