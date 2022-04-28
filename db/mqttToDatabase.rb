9c57ea2f69f707ef8d65c064ebf8713356769cf3
imthegngrbrdmn <maarnold7@alaska.edu>
2022-04-28 14:28:11 -0800

    pg

--- db/mqttToDatabase.rb
+++ db/mqttToDatabase.rb
@@ -22,15 +22,15 @@ end
  battery_query = db.exec "SELECT battery FROM microgrids WHERE name= 'Kotzebue'"
  $battery_message = battery_query.values[0][0]
  wind_query = db.exec "SELECT wind FROM microgrids WHERE name= 'Kotzebue'"
- $wind_message = wind_query.values[0]
+ $wind_message = wind_query.values[0][0]
  solar_query = db.exec "SELECT solar FROM microgrids WHERE name= 'Kotzebue'"
- $solar_message = solar_query.values[0]
+ $solar_message = solar_query.values[0][0]
  diesel_query = db.exec "SELECT diesel FROM microgrids WHERE name= 'Kotzebue'"
- $diesel_message = diesel_query.values[0]
+ $diesel_message = diesel_query.values[0][0]
  total_load_query = db.exec "SELECT total_load FROM microgrids WHERE name= 'Kotzebue'"
- $total_load = total_load_query.values[0]
+ $total_load = total_load_query.values[0][0]
  updated_query = db.exec "SELECT updated_at FROM microgrids WHERE name= 'Kotzebue'"
- $updated = updated_query.values[0]
+ $updated = updated_query.values[0][0]
 
 if $wind_message == nil
   $wind_message, $solar_message, $diesel_message, $battery_message = 0
