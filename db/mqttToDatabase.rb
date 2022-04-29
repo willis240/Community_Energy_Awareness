b03933b955ba590ee753752743c655ffa025e930
imthegngrbrdmn <maarnold7@alaska.edu>
2022-04-28 16:49:47 -0800

    pg

--- db/mqttToDatabase.rb
+++ db/mqttToDatabase.rb
@@ -64,6 +64,7 @@ $wind_percentage = (($wind_message / $total_load.to_f) * 100).round(2)
 $solar_percentage = (($solar_message / $total_load.to_f) * 100).round(2)
 $diesel_percentage = (($diesel_message / $total_load.to_f) * 100).round(2)
   $updated = DateTime.now.to_s
+  $updated["T"] = " "
   
   db.exec "UPDATE microgrids SET (battery, wind, solar, diesel, total_load, updated_at) = (%d, %d, %d, %d, %d, %s) WHERE name = 'Kotzebue'" % [$battery_message, $wind_message, $solar_message, $diesel_message, $total_load, $updated]
 
